641c8e9786ea3d88f61fd786afba36b323ff7177
Christoph Schreiber <schreiber@pluspol.info>
2013-09-03 13:18:51 +0200

    TemplatePage. Added options to set contentType, responseCode, baseTemplate.

--- modules/BootstrapModule/source/Pages/TemplatePage.php
+++ modules/BootstrapModule/source/Pages/TemplatePage.php
@@ -8,6 +8,12 @@ class TemplatePage extends Page
 {
     public function parseTask()
     {
-        $this->setResult(GlobalFactory::get($this->app)->appTemplates()->load($this->task->vars->get('template'))->content);
+        $this->setResult(
+            GlobalFactory::get($this->app)->appTemplates()->load($this->task->vars->get('template'),
+                null,
+                $this->task->vars->get('baseTemplate', 'base.html')
+            )->content,
+            $this->task->vars->get('contentType'),
+            $this->task->vars->get('responseCode'));
     }
 }
