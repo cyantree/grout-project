1a88ca7e64de4ae725a8e763fb3cb473d274077d
cyantree <mail@cyantree.com>
2015-01-29 00:14:05 +0100

    * AppLiveConfig: Added doctrine configuration for quicker start.

--- modules/AppModule/Configs/AppLiveConfig.php
+++ modules/AppModule/Configs/AppLiveConfig.php
@@ -1,6 +1,20 @@
 <?php
 namespace Grout\AppModule\Configs;
 
+use Grout\Cyantree\DoctrineModule\ConnectionDetails\PdoMySqlConnectionDetails;
+use Grout\Cyantree\DoctrineModule\Types\DoctrineConfig;
+
 class AppLiveConfig extends AppDevelopmentConfig
 {
+    public function configureCyantreeDoctrineModule(DoctrineConfig $config)
+    {
+        parent::configureCyantreeDoctrineModule($config);
+
+        /** @var PdoMySqlConnectionDetails $connectionDetails */
+        $connectionDetails = $config->connectionDetails;
+
+        $connectionDetails->username = 'root';
+        $connectionDetails->password = '';
+        $connectionDetails->database = '';
+    }
 }
